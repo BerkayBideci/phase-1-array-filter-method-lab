2f1b8fcf252655da88e959d42827178c444d7086
Berkay Bideci <berkaybideci@gmail.com>
2023-04-17 16:42:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, query) {
+    return array.filter((element) => element.toLowerCase() == query.toLowerCase())
+}
+
+function fuzzyMatch(array, query) {
+    return array.filter((element) => element.toLowerCase().startsWith(query[0].toLowerCase()))
+}
+
+function matchName(array, query) {
+    return array.filter((element) => element.name.toLowerCase() === query.toLowerCase())
+}
